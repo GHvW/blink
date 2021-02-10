e6b5282837869ac511398b06fe77d193a374ac6a
Garrett van Wageningen <ghvanw283@gmail.com>
2021-02-09 23:44:07 -0600

    set up env

--- src/main/blink/app/main.cljs
+++ src/main/blink/app/main.cljs
@@ -1,5 +1,17 @@
-(ns blink.app.main)
+(ns blink.app.main
+  (:require
+   ["process" :as process]))
+
+
+(def api-token (.. process -env -BIBLE_API_KEY))
+
 
 (defn main
   []
   (js/console.log "hello world!"))
+
+
+(comment
+  api-token
+  
+  (+ 2 3))
