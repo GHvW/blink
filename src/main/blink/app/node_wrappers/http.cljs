f80a503699da70200e88a248b3f2d00fa3fa9ba2
Garrett van Wageningen <ghvanw283@gmail.com>
2021-02-11 19:26:01 -0600

    wip

--- src/main/blink/app/node_wrappers/http.cljs
+++ src/main/blink/app/node_wrappers/http.cljs
@@ -1,10 +1,15 @@
 (ns blink.app.node-wrappers.http
   (:require
    ["http" :as http]
-   [cljs.core.async :refer [go chan <! put!]]))
+   [cljs.core.async :refer [go chan <! put! close!]]))
 
 
-(defn get-request
+(defn deserialize
+  [data]
+  (.parse js/JSON data))
+
+
+(defn node-get
   ([url] (get url nil))
   ([url options]
   (let [out-chan (chan)]
@@ -12,12 +17,22 @@
               options
               (fn [response]
                 (let [status-code (.-statusCode response)]
-                  (if (not= status-code)
-                    (put! out-chan [status-code "error"])
-                    ())))))))
+                  (if (not= status-code 200)
+                    (do
+                      (put! out-chan [:error {:code status-code :message "error"}])
+                      (.resume response))
+                    (do
+                      (.on response "data" (fn [chunk] (put! [:data chunk])))
+                      (.on response "end" (fn [] (close! out-chan))))))))
+    out-chan))
 
 
 
 (comment
   (not= 1 2)
+
+  (deserialize "{ \"person\": { \"name\": \"Garrett\" } }")
+
+  (js->clj (deserialize "{ \"person\": { \"name\": \"Garrett\" } }"))
+
   (+ 1 2))
